506ca27fe4199816924dd159ed1d781b3c5317e8
nagavath <pavannagavathlove123@gmail.com>
2025-08-04 19:54:43 +0530

    Initial portfolio upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var Typed= new Typed (".text", {
+    strings:["Web Developer", "Frontend Developer", "Backend Developer","SQL Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1500,
+    loop:true
+}
+);
